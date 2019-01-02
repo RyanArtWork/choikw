47043961d976391396c5578db840c846c26e12cc
RyanArtWork <ryanartwork@gmail.com>
2019-01-02 16:42:54 +0800

    v34

--- js.js
+++ js.js
@@ -12,11 +12,39 @@ window.onscroll = function() {scrollFunction()};
       document.getElementById("bnav").style.padding= "0px 0px";
       document.getElementById("bnav").style.lineHeight= "300px";
       document.getElementById("bnav").style.fontSize= "1.5rem";
-
     }
   }
 
-
-
+		
+  $(document).ready(function() {
+		
+		var scrollLink = $('.scroll');
 	
+		// Smooth scrolling
+		scrollLink.click(function(e) {
+		e.preventDefault();
+		$('body,html').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 100 );
+		});
+		
+		// Active link switching
+		$(window).scroll(function() {
+		var scrollbarLocation = $(this).scrollTop();
+		
+		scrollLink.each(function() {
+			
+			var sectionOffset = $(this.hash).offset().top - 300;
+			
+			if ( sectionOffset <= scrollbarLocation ) {
+			$(this).parent().addClass('active');
+			$(this).parent().siblings().removeClass('active');
+			}
+		})
+		
+		})
+		
+	})	
+
+
 	
